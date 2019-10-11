68ffc2af83b3383e86d66354802dfab0ff6549d9
sagdish <forsi1@yahoo.com>
2019-10-11 17:00:29 +0500

    integrating contex api state management

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
+import { DataProvider } from './components/DataContext';
 import TaskList from './components/TaskList/TaskList';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <TaskList />
-    </div>
+    <DataProvider>
+      <div className="App">
+        <TaskList />
+      </div>
+    </DataProvider>
   );
 }
 
---
+++ src/components/DataContext.js
@@ -0,0 +1,45 @@
+import React, { useState, createContext, useEffect } from 'react'
+import axios from 'axios';
+
+export const DataContext = createContext();
+
+async function getTasks() {
+  try {
+    const response = await axios.get("http://intravision-task.test01.intravision.ru/odata/tasks?tenantguid=fb46e052-aee1-49c0-a975-5be572d900ef")
+    // console.log(response);
+    return response.data
+  }
+  catch(err) {
+    console.error("error fetching places: ", err)
+  }
+}
+
+
+export const DataProvider = (props) => {
+  const [data, setData] = useState({tasks: [], isLoading: true});
+
+  useEffect(() => {
+    getTasks()
+      .then(response => 
+          response.value.map(task => {
+          // console.log(task);
+          const { id, name, description, statusName, statusRgb, executorName } = task;
+          // console.log(id, name, description, statusName, statusRgb, executorName)
+          return { id, name, description, statusName, statusRgb, executorName };
+        })
+      )
+      .then(tasks => {
+        console.log(tasks);
+        setData({tasks, isLoading: false});
+      })
+      .catch(err => console.log("error in effect hook: ", err));
+  }, [])
+
+  return (
+    <div>
+      <DataContext.Provider value={[ data, setData ]}>
+        {props.children}
+      </DataContext.Provider>
+    </div>
+  )
+}
